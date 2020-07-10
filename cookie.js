97152d7a1df95e5fee14e14e07e56327b923705c
Mauricio Cifuentes <mauricio@devrocks.de>
2020-07-10 22:45:41 +0200

    agb'

---
+++ cookie.js
@@ -0,0 +1,7 @@
+<div>
+  Wir verwenden Cookies, um die einwandfreie Funktion unserer Website zu
+  gewährleisten, unseren Datenverkehr zu analysieren, Werbung zu personalisieren
+  und um SPAM zu verhindern. Wir informieren auch unsere Analyse- und
+  Werbepartner über Nutzeraktivitäten auf der Website. Einstellungen verwalten »
+  Empfehlung akzeptieren
+</div>;
